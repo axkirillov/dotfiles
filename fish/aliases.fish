c188380c6645554f3d1a618b05fcb28638d2345b
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2022-08-01 16:25:19 +0200

    updates

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,21 +1,15 @@
 # aliases
 alias 9=k9s
 alias delete-released-branches="git branch --merged | egrep -v '(^\*|master|develop)' | xargs git branch -d"
-alias e="edit $FISH_CONFIG"
 alias h="cd $HOME"
 alias k="kubectl"
-alias lg="CONFIG_DIR=~/.config/lazygit lazygit"
 alias ls="lsd"
-alias n="nnn -e"
-alias r="gh copr -r"
 alias ssh-add-rsa="ssh-add $HOME/.ssh/id_rsa"
 alias update-kitty="curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin"
-alias vimrc="v $HOME/.vimrc"
 alias v=nvim
 alias vi=nvim
 alias vim=nvim
 alias cfg='git --git-dir=$HOME/.cfg/ --work-tree=/Users/aleksandr.kirillov'
-alias init-dot-vim="v $HOME/.config/nvim/init.vim"
 alias rm="rm -i"
 alias request="make -C $HOME/requests/"
 alias requests="vi $HOME/requests/Makefile" 
--- fish/globals.fish
+++ fish/globals.fish
@@ -1,10 +1,9 @@
 # global vars
 set -xg CONFIG $HOME/.config
 set -xg FISH_CONFIG $CONFIG/fish/config.fish
-set -xg VIM_CONFIG $HOME/.vim/config
-set -xg NVCFG $CONFIG/nvim
+set -xg NVIM_CONFIG $CONFIG/nvim
 set -xg EDITOR nvim
 set -xg VISUAL $EDITOR
 set -xg FZF_DEFAULT_COMMAND "fd --type f -H"
+# needed for ruby integration with neovim
 set -xg GEM_HOME $HOME/.gem/bin/
-set -xg SCRIPTS $HOME/scripts
